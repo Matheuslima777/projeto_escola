6441fb9de14a5edacb163e0cb11ee45d0c599ed6
Matheuslima777 <matheuxlima77@gmail.com>
2024-10-16 15:29:48 -0300

    mudança de arquivos

--- escola.c
+++ escola.c
@@ -1,15 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "utils.h"
 
-#define TAM_ALUNO 3
-#define CAD_ALUNO_SUCESSO -1
-#define MATRICULA_INVALIDA -2
-#define LISTA_CHEIA -3
-#define ATUALIZADO_ALUNO_SUCESSO -4
-#define MATRICULA_INEXISTENTE -5
-#define EXCLUSAO_ALUNO_SUCESSO -6
 
-//mudando denovo para o segundo commit biruleibe
+
+//mudando denovo para o segundo commit biruleib
 
 
 
