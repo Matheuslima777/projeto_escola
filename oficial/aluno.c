52dc969c612f347500d9caa50b18440acebd220a
Cauazni <cauasapsilva@gmail.com>
2024-10-29 21:23:57 -0300

    Update aluno.c

--- oficial/aluno.c
+++ oficial/aluno.c
@@ -69,8 +69,8 @@ int cadastrarAluno(int qtdAluno, Aluno listarAluno[]) {
     char sexo;
     int i;
     int matriculaaluno;
-    
-    
+
+
 
     printf("DIGITE SEU NOME:\n");
     getchar();
@@ -91,13 +91,13 @@ int cadastrarAluno(int qtdAluno, Aluno listarAluno[]) {
 
       if (retorno_verificador_aluno == ALUNO_EXISTE) {
         printf("Já existe um aluno com essa matrícula\n");
-        
+
       } else if (retorno_verificador_aluno == MATRICULA_ALUNO_INEXISTENTE) {
         printf("Matricula do aluno é válida\n");
         achoualuno = 1;
       }
     }
-    
+
     int cpfvalido = 0;
     while(cpfvalido == 0){
       printf("DIGITE O CPF DO ALUNO:(formato: 000.000.000-00)\n");
@@ -106,12 +106,12 @@ int cadastrarAluno(int qtdAluno, Aluno listarAluno[]) {
       if (retorno_cpf == CPF_ALUNO_JA_CADASTRADO) {
         printf("Já existe um aluno com esse CPF\n");
       }
-        
+
       else if(retorno_cpf == CPF_ALUNO_INVALIDO)
       {
         printf("CPF do aluno inválido\n");
       }
-        
+
       else if (retorno_cpf== CPF_ALUNO_VALIDO)
       {
         printf("CPF do aluno é válida\n");
@@ -148,7 +148,7 @@ int cadastrarAluno(int qtdAluno, Aluno listarAluno[]) {
     // Calcular idade
     idade = anoAtual - anoNascimento;
 
-    
+
     if (sexo == 'F' || sexo == 'M') {
 
       // nome do aluno
@@ -184,7 +184,7 @@ int cadastrarAluno(int qtdAluno, Aluno listarAluno[]) {
     }
     else{
       return SEXO_ALUNO_INVALIDO;
-      
+
     }
   }
 }
@@ -209,7 +209,7 @@ int verificarAluno(int qtdAluno, Aluno listarAluno[], int *matriculaaluno) {
 int verificarCpfaluno(int qtdAluno, Aluno listarAluno[], char *cpf){
     getchar();
     scanf("%14s", cpf);
-  
+
     size_t tamanho_cpf = strlen(cpf);
     // Verifica se o comprimento do CPF é 14
     if (tamanho_cpf != 14) {
@@ -272,133 +272,161 @@ void listarrAlunos(int qtdAluno, Aluno listarAluno[]) {
 int atualizarAluno(int qtdAluno, Aluno listarAluno[]) {
   printf("ATUALIZAR ALUNO\n");
   int matricula;
+  int achoualuno = 0;
+  while (achoualuno == 0)
+  {
+    printf("DIGITE A MATRICULA DO ALUNO:\n");
+    int retorno_verificador_aluno =
+        verificarAluno(qtdAluno, listarAluno, &matricula);
+    if(retorno_verificador_aluno == ALUNO_EXISTE){
+      for (int i = 0; i < qtdAluno; i++)
+      {
+        if (matricula == listarAluno[i].matricula && listarAluno[i].ativo) {
+          // atualização
+          int novaMatricula;
+          char novoNome[20];
+          int newdiaNascimento;
+          int newanoNascimento;
+          int newmesNascimento;
+          char newSexo;
+          char novoCpf[15];
 
-  printf("DIGITE A MATRICULA:\n");
-  scanf("%d", &matricula);
-  int achou = 0;
-  if (matricula < 0) {
-    return MATRICULA_ALUNO_INVALIDA;
-  } else {
-    for (int i = 0; i < qtdAluno; i++) {
-      if (matricula == listarAluno[i].matricula && listarAluno[i].ativo) {
-        // atualização
-        int novaMatricula;
-        char novoNome[20];
-        int newdiaNascimento;
-        int newanoNascimento;
-        int newmesNascimento;
-        char newSexo;
-        char novoCpf[15];
+          int achoualuno = 0;
+          while (achoualuno == 0)
+          {
+            printf("DIGITE A NOVA MATRICULA DO ALUNO:\n");
+            int retorno_verificador_aluno =
+                verificarAluno(qtdAluno, listarAluno, &novaMatricula);
 
-        printf("DIGITE A NOVA MATRICULA:\n");
-        scanf("%d", &novaMatricula);
-        int achoualuno = 0;
-        while (achoualuno == 0) {
-          printf("DIGITE A MATRICULA DO ALUNO:\n");
-          int retorno_verificador_aluno =
-              verificarAluno(qtdAluno, listarAluno, &novamatricula);
+            if (retorno_verificador_aluno == ALUNO_EXISTE) {
+              printf("Já existe um aluno com essa matrícula\n");
 
-          if (retorno_verificador_aluno == ALUNO_EXISTE) {
-            printf("Já existe um aluno com essa matrícula\n");
-            
-          } else if (retorno_verificador_aluno == MATRICULA_ALUNO_INEXISTENTE) {
-            printf("Matricula do aluno é válida\n");
-            achoualuno = 1;
+            } else if (retorno_verificador_aluno == MATRICULA_ALUNO_INEXISTENTE) {
+              printf("Matricula do aluno é válida\n");
+              achoualuno = 1;
+            }
           }
+
+          printf("NOME:\n");
+          getchar();
+          fgets(novoNome, sizeof(novoNome), stdin);
+
+          size_t len = strlen(novoNome);
+          if (len > 0 && novoNome[len - 1] == '\n') {
+            novoNome[len - 1] = '\0';
+          }
+
+          int cpfvalido = 0;
+          while(cpfvalido == 0){
+            printf("DIGITE O NOVO CPF DO ALUNO:(formato: 000.000.000-00)\n");
+            int retorno_cpf = verificarCpfaluno(qtdAluno, listarAluno, novoCpf);
+
+            if (retorno_cpf == CPF_ALUNO_JA_CADASTRADO) {
+              printf("Já existe um aluno com esse CPF\n");
+            }
+
+            else if(retorno_cpf == CPF_ALUNO_INVALIDO)
+            {
+              printf("CPF do aluno inválido\n");
+            }
+
+            else if (retorno_cpf== CPF_ALUNO_VALIDO)
+            {
+              printf("CPF do aluno é válida\n");
+              cpfvalido = 1;
+            }
+          }
+
+          int data_certa =0;
+          while(data_certa == 0)
+          {
+            printf("DIGITE SUA DATA DE NASCIMENTO NOVA(formato: DD/MM/AAAA):\n");
+            int retorno_data = validar_data(newdiaNascimento, newmesNascimento, newanoNascimento);
+            if(retorno_data == DATA_VALIDA)
+            { 
+              printf("Data válida!\n");
+              data_certa = 1;
+            }
+            else if(retorno_data == DATA_INVALIDA)
+              printf("Data inválida!\n");
+          }
+          
+          printf("SEXO (M/F):\n");
+          scanf(" %c", &newSexo);
+
+          if (novaMatricula < 0) {
+            return MATRICULA_ALUNO_INVALIDA;
+          }
+          listarAluno[i].matricula = novaMatricula;
+          strcpy(listarAluno[i].nome, novoNome);
+          strcpy(listarAluno[i].cpf, novoCpf);
+          listarAluno[i].diaNascimento = newdiaNascimento;
+          listarAluno[i].mesNascimento = newmesNascimento;
+          listarAluno[i].anoNascimento = newanoNascimento;
+          listarAluno[i].sexo = newSexo;
+          break;
         }
-
-        printf("NOME:\n");
-        getchar();
-        fgets(novoNome, sizeof(novoNome), stdin);
-
-        size_t len = strlen(novoNome);
-        if (len > 0 && novoNome[len - 1] == '\n') {
-          novoNome[len - 1] = '\0';
-        }
-
-        printf("DIGITE O CPF:\n");
-        getchar();
-        fgets(novoCpf, sizeof(novoCpf), stdin);
-
-        len = strlen(novoCpf);
-        if (len > 0 && novoCpf[len - 1] == '\n') {
-          novoCpf[len - 1] = '\0';
-        }
-
-        printf("DIGITE SUA DATA DE NASCIMENTO (formato: DD/MM/AAAA):\n");
-        scanf("%d/%d/%d", &newdiaNascimento, &newmesNascimento,
-              &newanoNascimento);
-
-        printf("SEXO:\n");
-        scanf(" %c", &newSexo);
-
-        if (novaMatricula < 0) {
-          return MATRICULA_ALUNO_INVALIDA;
-        }
-        listarAluno[i].matricula = novaMatricula;
-        strcpy(listarAluno[i].nome, novoNome);
-        strcpy(listarAluno[i].cpf, novoCpf);
-        listarAluno[i].diaNascimento = newdiaNascimento;
-        listarAluno[i].mesNascimento = newmesNascimento;
-        listarAluno[i].anoNascimento = newanoNascimento;
-        listarAluno[i].sexo = newSexo;
-        achou = 1;
-        break;
       }
     }
-    if (achou) {
+    else if (retorno_verificador_aluno == MATRICULA_ALUNO_INEXISTENTE) {
+      printf("Matricula do aluno é válida\n");
+      }
+    if (achoualuno) {
       return ATUALIZADO_ALUNO_SUCESSO;
-
     } else {
       return MATRICULA_ALUNO_INEXISTENTE;
     }
   }
 }
 
-int excluirAluno(int qtdAluno, Aluno listarAluno[]) {
+int excluirAluno(int qtdAluno, Aluno listarAluno[])
+{
   printf("DELETAR ALUNO\n");
-  printf("DIGITE A MATRICULA:\n");
   int matricula;
-  scanf("%d", &matricula);
-  int achou = 0;
-  if (matricula < 0) {
-    return MATRICULA_ALUNO_INVALIDA;
-  } else {
-    for (int i = 0; i < qtdAluno; i++) {
-      if (matricula == listarAluno[i].matricula) {
+  int achoualuno = 0;
+  while (achoualuno == 0)
+  {
+    printf("DIGITE A NOVA MATRICULA DO ALUNO:\n");
+    int retorno_verificador_aluno = verificarAluno(qtdAluno, listarAluno, &matricula);
+    if (retorno_verificador_aluno == ALUNO_EXISTE) {
+      for (int i = 0; i < qtdAluno; i++)
+      {
+        if (matricula == listarAluno[i].matricula) {
 
-        // exclusão logica
-        listarAluno[i].ativo = -1;
+          // exclusão logica
+          listarAluno[i].ativo = -1;
 
-        for (int j = i; j < qtdAluno - 1; j++) {
-          // movendo todos os alunos pra casa anterior
-          listarAluno[j].nome[20] = listarAluno[j + 1].nome[20];
-          listarAluno[j].cpf[15] = listarAluno[j + 1].cpf[15];
-          listarAluno[j].matricula = listarAluno[j + 1].matricula;
-          listarAluno[j].sexo = listarAluno[j + 1].sexo;
-          listarAluno[j].ativo = listarAluno[j + 1].ativo;
+          for (int j = i; j < qtdAluno - 1; j++) {
+            // movendo todos os alunos pra casa anterior
+            listarAluno[j].nome[20] = listarAluno[j + 1].nome[20];
+            listarAluno[j].cpf[15] = listarAluno[j + 1].cpf[15];
+            listarAluno[j].matricula = listarAluno[j + 1].matricula;
+            listarAluno[j].sexo = listarAluno[j + 1].sexo;
+            listarAluno[j].ativo = listarAluno[j + 1].ativo;
 
-          // movendo as datas de cadastro e de nascimento
-          // nascimento
-          listarAluno[j].diaNascimento = listarAluno[j + 1].diaNascimento;
-          listarAluno[j].mesNascimento = listarAluno[j + 1].mesNascimento;
-          listarAluno[j].anoNascimento = listarAluno[j + 1].anoNascimento;
-          // cadastro
-          listarAluno[j].diaAtual = listarAluno[j + 1].diaAtual;
-          listarAluno[j].mesAtual = listarAluno[j + 1].mesAtual;
-          listarAluno[j].anoAtual = listarAluno[j + 1].anoAtual;
+            // movendo as datas de cadastro e de nascimento
+            // nascimento
+            listarAluno[j].diaNascimento = listarAluno[j + 1].diaNascimento;
+            listarAluno[j].mesNascimento = listarAluno[j + 1].mesNascimento;
+            listarAluno[j].anoNascimento = listarAluno[j + 1].anoNascimento;
+            // cadastro
+            listarAluno[j].diaAtual = listarAluno[j + 1].diaAtual;
+            listarAluno[j].mesAtual = listarAluno[j + 1].mesAtual;
+            listarAluno[j].anoAtual = listarAluno[j + 1].anoAtual;
+          }
+
+          achoualuno = 1;
+          break;
         }
-
-        achou = 1;
-        break;
       }
     }
-
-    if (achou) {
-      return EXCLUSAO_ALUNO_SUCESSO;
-
-    } else {
+    else if(retorno_verificador_aluno == MATRICULA_ALUNO_INEXISTENTE) {
       return MATRICULA_ALUNO_INEXISTENTE;
     }
+  }
+  if (achoualuno) {
+    return EXCLUSAO_ALUNO_SUCESSO;
+  } else {
+    return MATRICULA_ALUNO_INEXISTENTE;
   }
 }
